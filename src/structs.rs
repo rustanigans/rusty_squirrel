b5ac86e7637ae5ceb1429890208c219bc8ed6b2c
Sibz <12050358+Sibz@users.noreply.github.com>
2021-10-24 09:49:16 +1100

    refactor: removed helper fn module

--- src/structs.rs
+++ src/structs.rs
@@ -1,8 +1,5 @@
 pub mod database_internal;
-mod db_int_helper;
-/*structs::db_int_helper::{internal_delete_by_expression, internal_delete_by_id, internal_insert,
-internal_query_by_expression, internal_query_by_id, internal_update_column,
-internal_update_item},*/
+
 use crate::traits::{Insertable, Updatable, *};
 use anyhow::{anyhow, Result};
 use mysql::{Pool, PooledConn};
