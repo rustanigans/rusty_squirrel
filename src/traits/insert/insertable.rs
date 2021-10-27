b688bcfdb9d8b5b93599a82a0bbfc0da60e898e8
Sibz <12050358+Sibz@users.noreply.github.com>
2021-10-27 16:49:08 +1100

    fix: add ticks to field name

--- src/traits/insert/insertable.rs
+++ src/traits/insert/insertable.rs
@@ -3,9 +3,7 @@ use mysql::Params;
 
 pub trait Insertable: Table + Send + Sync
 {
-    // TODO move this to collection
     fn to_params(&self) -> mysql::Params;
-    // TODO move this to collection
     fn insert_expr(&self) -> String
     {
         let mut field_names_a = vec![];
@@ -14,7 +12,7 @@ pub trait Insertable: Table + Send + Sync
         {
             for field_name in params.keys()
             {
-                field_names_a.push(field_name.to_string());
+                field_names_a.push(format!("`{}`", field_name));
                 field_names_b.push(format!(":{}", field_name));
             }
         }
