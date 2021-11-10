1e56a1e893f1db2b29869f8e6def4c492e4e589e
Sibz <12050358+Sibz@users.noreply.github.com>
2021-11-10 17:05:34 +1100

    feat!: add type params to create/delete table traits

--- src/traits/table/table_create.rs
+++ src/traits/table/table_create.rs
@@ -4,11 +4,11 @@ use mysql::prelude::Queryable;
 
 pub trait TableCreate<T: Table>: CollectionViewInterface<T>
 {
-    fn create_table_statement(&self) -> String;
-    fn create_new_table(&self) -> Result<()>
+    fn create_table_statement<TABLE: Table>(&self) -> String;
+    fn create_new_table<TABLE: Table>(&self) -> Result<()>
     {
         self.get_connection()?
-            .query_drop(&self.create_table_statement())
-            .map_err(|e| anyhow::anyhow!("Unable To Create Table - '{}' - {}", T::TABLE_NAME, e))
+            .query_drop(&self.create_table_statement::<TABLE>())
+            .map_err(|e| anyhow::anyhow!("Unable To Create Table - '{}' - {}", TABLE::TABLE_NAME, e))
     }
 }
--- src/traits/table/table_delete.rs
+++ src/traits/table/table_delete.rs
@@ -4,10 +4,10 @@ use mysql::prelude::Queryable;
 
 pub trait TableDelete<T: Table>: CollectionViewInterface<T>
 {
-    fn delete_table(&self) -> Result<()>
+    fn delete_table<TABLE: Table>(&self) -> Result<()>
     {
         self.get_connection()?
-            .query_drop(&format!("DROP TABLE {}", T::TABLE_NAME))
-            .map_err(|e| anyhow::anyhow!("Unable To Delete Table - '{}' - {}", T::TABLE_NAME, e))
+            .query_drop(&format!("DROP TABLE {}", TABLE::TABLE_NAME))
+            .map_err(|e| anyhow::anyhow!("Unable To Delete Table - '{}' - {}", TABLE::TABLE_NAME, e))
     }
 }
