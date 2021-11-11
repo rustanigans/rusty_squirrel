f3d16715f047823a6b517f46a07dfc149121ea42
Sibz <12050358+Sibz@users.noreply.github.com>
2021-11-11 12:19:34 +1100

    feat: add query drop fn

--- src/traits/get_database.rs
+++ src/traits/get_database.rs
@@ -1,7 +1,11 @@
 use anyhow::Result;
-use mysql::PooledConn;
+use mysql::{prelude::Queryable, PooledConn};
 
 pub trait GetDatabase: Send + Sync
 {
     fn get_connection(&self) -> Result<PooledConn>;
+    fn query_drop(&self, sql: &str) -> Result<()>
+    {
+        Ok(self.get_connection()?.query_drop(sql)?)
+    }
 }
