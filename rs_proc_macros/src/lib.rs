e48941bb3241c2dd27b90140bd10e99127bb3771
Tzian <1164336+Tzian@users.noreply.github.com>
2021-11-13 08:21:46 +0000

    feature(RustyEnum): added derive macro

--- rs_proc_macros/src/lib.rs
+++ rs_proc_macros/src/lib.rs
@@ -87,3 +87,50 @@ pub fn derive(input: TokenStream) -> TokenStream
         derive_error!(ast, "Expected Struct");
     }
 }
+
+#[proc_macro_derive(RustyEnum)]
+pub fn from_enum(input: TokenStream) -> TokenStream
+{
+    let ast: DeriveInput = parse_macro_input!(input as DeriveInput);
+
+    if let Data::Enum(e) = &ast.data
+    {
+        use convert_case::{Case, Casing};
+        let mod_name = format_ident!("impl_{}", ast.ident.to_string().to_case(Case::Snake));
+
+        let mut extended = quote! {};
+        for v in &e.variants
+        {
+            let variant_type = &v;
+
+            extended.append_all(quote! {
+                        mod #mod_name
+                        {
+                            use super::*;
+                            use anyhow::Error;
+
+                            impl TryFrom<u8> for #variant_type //(enums field)
+                            {
+                                type Error = anyhow::Error;
+
+                                fn try_from(value: u8) -> Result<Self, Self::Error>
+                                {
+                                    Ok((match value
+                                        {
+                                            (x if x == <self>::#variant_type as u8 => <self>::#variant_type,)*
+                                            _ => anyhow::bail!("Invalid Enum Value")
+                                        }))
+                                }
+
+                            }
+                        }
+                    });
+        }
+        println!("{}", extended);
+        extended.into()
+    }
+    else
+    {
+        derive_error!(ast, "Expected Enum");
+    }
+}
