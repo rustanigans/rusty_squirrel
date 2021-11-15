c526aaa5dff1b9260bd16960fa9a7e35c8f7dbff
Sibz <12050358+Sibz@users.noreply.github.com>
2021-11-15 14:34:03 +1100

    fix: add cfg attr decorations

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,19 @@
 #![allow(clippy::suspicious_else_formatting)]
 
+#[cfg(feature = "default")]
 pub use structs::database_internal::{SquirrelDatabase, SquirrelDatabaseConfig};
+
 // Backwards compatibility
+#[cfg(feature = "default")]
 pub use SquirrelDatabase as DatabaseInternal;
 
+#[cfg(feature = "default")]
 #[macro_use]
 extern crate anyhow;
 
+#[cfg(feature = "default")]
 mod structs;
+#[cfg(feature = "default")]
 pub mod traits
 {
     pub use collections::*;
@@ -22,10 +28,14 @@ pub mod traits
     mod taker;
     mod view;
 }
+
+#[cfg(feature = "default")]
 pub mod macros
 {
     pub mod enum_u8;
 }
-
+#[cfg(feature = "default")]
 pub const MYSQL_DATE_FORMAT: &str = "%Y-%m-%d %H:%M:%S";
+
+#[cfg(feature = "rs_proc_macros")]
 pub use rs_proc_macros::{RustyEnum, RustyParams};
